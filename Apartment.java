8f0f7cbe636d89fc510802d62a045d5b8aa90e79
soozijan <75090137+soozijan@users.noreply.github.com>
2021-03-16 18:36:22 +0200

    Create Apartment.java

---
+++ Apartment.java
@@ -0,0 +1,15 @@
+package TirgulPackage;
+
+
+public class Apartment {
+    protected String adress;
+    protected float size;
+
+    @Override
+    public String toString() {
+        return "Apartment{" +
+                "adress='" + adress + '\'' +
+                ", size=" + size +
+                '}';
+    }
+}
