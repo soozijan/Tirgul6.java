bdd795e21ecc57e8e29cb2bbaac90141d6f81a39
soozijan <75090137+soozijan@users.noreply.github.com>
2021-03-16 19:13:01 +0200

    Create Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,14 @@
+package TirgulPackage;
+
+import lombok.ToString;
+
+@ToString
+public class Teacher {
+   protected String name;
+    protected int id_number;
+
+    public Teacher(String name, int id_number) {
+        this.name = name;
+        this.id_number = id_number;
+    }
+}
