a5dd921c222abd032d815a14c8192c846209800f
soozijan <75090137+soozijan@users.noreply.github.com>
2021-03-16 19:08:18 +0200

    Create Orange.java

---
+++ Orange.java
@@ -0,0 +1,11 @@
+package TirgulPackage;
+
+public class Orange extends Fruit {
+
+    @Override
+    public void eat() {
+        System.out.println("...pealing the orange and eating");
+    }
+
+
+}
