45601f6439d16db052621b7120b7bff178609956
soozijan <75090137+soozijan@users.noreply.github.com>
2021-03-16 19:06:40 +0200

    Create MathTeacher.java

---
+++ MathTeacher.java
@@ -0,0 +1,12 @@
+package TirgulPackage;
+
+
+public class MathTeacher extends Teacher {
+   protected int math_skills;
+
+
+   public MathTeacher(String name, int id_number, int math_skills) {
+      super(name, id_number);
+      this.math_skills = math_skills;
+   }
+}
